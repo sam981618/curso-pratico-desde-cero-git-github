1ef381bcf069f09464c6e848361e1956d04dfc4e
sam9810 <samuelgarces203@gmail.com>
2022-09-22 21:53:49 -0500

    nuevos conceptos de ES6

--- pruebas.js
+++ pruebas.js
@@ -29,3 +29,43 @@ console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world} you are the man`;
 console.log(epicPhrase2);
 
+//multiple lines es5
+
+let lorem = "hola que hace joven un requisa porfavor \n"
++"otra requisita porfavor"
+console.log(lorem);
+//es6
+let lorem2 = `otra requisita aqui caballero
+y una mas aqui ilustrisimo`;
+console.log(lorem2);
+
+// destructuracion de elementos es5
+
+let person = {
+    'name': 'oscar',
+    'age': 25,
+    'country': 'MX'
+}
+
+console.log(person.name, person.age);
+
+// es6
+
+let { name, age, country } = person;
+console.log(name, age, country);
+
+// spread operator
+
+let team1 = ['oscar', 'julian', 'ricardo'];
+let team2 = ['valeria', 'jesica', 'camila'];
+let education = ['david', ...team1, ...team2];
+
+console.log(education);
+//es5
+let name1 = 'tito';
+let age2 = 26;
+
+Obj = {name1: name1, age2: age2};
+//es6
+obj2 = {name1, age2};
+console.log(obj2);
